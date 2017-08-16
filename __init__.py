4b951c60390f44a011c473e6ac892fcbb7f93854
Simon Voordouw <simonvoordouw@gmail.com>
2017-08-16 12:26:56 +0200

    Add plugin.json

--- __init__.py
+++ __init__.py
@@ -6,15 +6,7 @@ from UM.i18n import i18nCatalog
 i18n_catalog = i18nCatalog("cura")
 
 def getMetaData():
-    return {
-        "plugin": {
-            "name": "Doodle3D WiFi-box print",
-            "author": "Doodle3D",
-            "version": "2.0",
-            "api": 3,
-            "description": i18n_catalog.i18nc("@info:whatsthis","Accepts G-Code and sends them over WiFi to a Doodle3D WiFi-Box.")
-        }
-    }
+    return {}
 
 def register(app):
     return { "output_device": D3DCloudPrintPlugin.D3DCloudPrintPlugin() }
