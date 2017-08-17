217eaf694abb722a15bfdeae1a15b26124c781e6
Rick Companje <rick@companje.nl>
2017-08-17 17:32:08 +0200

    folder structure

--- Doodle3D/ConnectPrinterIdTranslation.py
+++ Doodle3D/ConnectPrinterIdTranslation.py
@@ -1,8 +1,8 @@
 # Translate a machine definition id to a Doodle3D Connect printer id
 # \return Doodle3D Connect printer id or the original
 def curaPrinterIdToConnect(id):
-    return {
-        "rigidbot": "rigidbot",
+	return {
+	"rigidbot": "rigidbot",
 	"ultimaker": "ultimaker",
 	"ultimaker2": "ultimaker2",
 	"ultimaker2_go": "ultimaker2go",
@@ -13,5 +13,5 @@ def curaPrinterIdToConnect(id):
 	"prusa_i3": "prusa_i3",
 	"prusa_i3_mk2": "prusa_iteration_2",
 	"robo_3d_r1": "robo_3d_printer",
-        "renkforce_rf100": "renkforce_rf100"
-    }.get(id, id)
+	"renkforce_rf100": "renkforce_rf100"
+}.get(id, id)
