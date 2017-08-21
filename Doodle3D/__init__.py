7a56fc609d5fe1ce7e4afd9a6649812337618be2
Simon Voordouw <simonvoordouw@gmail.com>
2017-08-21 18:10:48 +0200

    Add "Open Doodle3D Connect" menu item

--- Doodle3D/__init__.py
+++ Doodle3D/__init__.py
@@ -1,10 +1,14 @@
-# Copyright (c) 2015 Ultimaker B.V.
+# Copyright (c) 2017 Doodle3D.
 # Cura is released under the terms of the AGPLv3 or higher.
 
+from . import D3DCloudPrintOutputDevicePlugin
 from . import D3DCloudPrintPlugin
 
 def getMetaData():
     return {}
 
 def register(app):
-    return { "output_device": D3DCloudPrintPlugin.D3DCloudPrintPlugin() }
+    return {
+        "extension": D3DCloudPrintPlugin.D3DCloudPrintPlugin(),
+        "output_device": D3DCloudPrintOutputDevicePlugin.D3DCloudPrintOutputDevicePlugin()
+    }
